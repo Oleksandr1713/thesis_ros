3bf4dfb908fa398b2ffe3e3d6b2feac491a97de2
OleksandrZ <oleksandr.zaloznyi@tu-dresden.de>
2020-09-07 23:06:56 +0200

    feat: add in-memory cache containing already detected and reflected on the static map signs
    (cherry picked from commit f6dac47b3a4e0b8f70a28561b31662f7acc6c540)

--- rosbot_description/src/simulation/include/cache/cache.h
+++ rosbot_description/src/simulation/include/cache/cache.h
@@ -34,22 +34,22 @@ public:
         std::cout << "Cache Has Been Emptied" << std::endl;
     };
 
-    void addElement(Obstacle& obstacle){
+    bool addElement(Obstacle& obstacle){
         int index = findElement(obstacle);
         if(index == -1){
             data.push_back(obstacle);
-            std::cout << "New element has been added" << std::endl;
+            return true;
         } else{
-            std::cout << "Element cannot be added, because it is already in the Cache" << std::endl;
+            return false;
         }
     }
 
-    void removeElement(int index){
+    bool removeElement(int index){
         if(index >= 0 && index < data.size()){
             data.erase(data.begin() + index);
-            std::cout << "Element has been removed" << std::endl;
+            return true;
         } else {
-            std::cout << "Error! Index outside of the Cache size" << std::endl;
+            return false;
         }
 
     }
