f6f5d11881afe6e1c32c77e5d3fabbbcdd21c9d8
OleksandrZ <oleksandr.zaloznyi@tu-dresden.de>
2021-01-09 18:48:14 +0100

    amend: setup to run test cases described in evaluation chapter

--- map_enhancement/simulation/include/constants/obstacle_constants.h
+++ map_enhancement/simulation/include/constants/obstacle_constants.h
@@ -4,7 +4,7 @@
 namespace obstacle_constants {
 
     static int const STOP_SIGN_ID = 9; // this id matches the id obtained using find_object_2d package during image recognition training
-    static long const STOP_SIGN_LIFESPAN = 1800; // seconds
+    static long const STOP_SIGN_LIFESPAN = 3600; // seconds
 
     static int const BLOCK_SIGN_ID = 2;
     static long const BLOCK_SIGN_LIFESPAN = 300; // seconds
